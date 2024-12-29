7afc5bc1c038a2808f3a55f54e38fccc27615f0f
null223 <sumidamao@gmail.com>
2024-12-30 08:49:18 +0900

    tweak

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -43,7 +43,7 @@ export async function getPlaylistItemsData(playlistId: string): Promise<ItemProp
         title: item.snippet.title,
         thumbnail: item.snippet.thumbnails.medium.url,
         url: `https://www.youtube.com/watch?${new URLSearchParams({
-          w: item.snippet.resourceId.videoId,
+          v: item.snippet.resourceId.videoId,
           list: playlistId,
           index: item.snippet.position
         })}`
