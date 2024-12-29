15e0bc371ed4ee03f0e45a032f12880631bf99b5
null223 <sumidamao@gmail.com>
2024-12-30 06:08:04 +0900

    fix color

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
       <div className="relative z-10 pt-16 pb-8 text-center">
         <h1 className="text-6xl font-black tracking-wider">
           PLAY:LIST
-          <span className="block text-2xl mt-2 text-red-500">by MOTIKEN</span>
+          <span className="block text-2xl mt-2 text-black">by <span className="text-[#FF931E]">MOTIKEN</span></span>
         </h1>
       </div>
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-red-500 rounded-t-[100px]" />
